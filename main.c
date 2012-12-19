98e5f8117bff9fab4a28f9f5bc9d5d047ac134d3
Nick Parkanyi <n.parkanyi@gmail.com>
2012-12-19 11:20:08 -0500

    surface type define

--- main.c
+++ main.c
@@ -1,30 +1,40 @@
+/* Nick Parkanyi, ICS3U Summative Project:
+ * "Omega" shooter game. */
+
+/* the surface type, software or hardware. Could be defined during compilation
+ * to be hardware instead of software (can be problematic on some systems) */
+#ifndef SDLSFTYPE
+#define SDLSFTYPE SDL_HWSURFACE|SDL_DOUBLEBUF
+#endif
 #include <stdlib.h>
 #include <SDL/SDL.h>
 
 int main ( int argc, char** argv )
 {
-    // initialize SDL video
+    int running;
+    int i;
+    /* start SDL's video functionality */
     if ( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
         printf( "Unable to init SDL: %s\n", SDL_GetError() );
         return 1;
     }
 
-    // make sure SDL cleans up before exit
     atexit(SDL_Quit);
 
-    // create a new window
-    SDL_Surface* screen = SDL_SetVideoMode(640, 480, 16,
-                                           SDL_HWSURFACE|SDL_DOUBLEBUF);
-    if ( !screen )
+    /* create a new window */
+    SDL_Surface * screen;
+    screen = SDL_SetVideoMode(640, 480, 16, SDLSFTYPE);
+    if (screen == NULL)
     {
         printf("Unable to set 640x480 video: %s\n", SDL_GetError());
         return 1;
     }
 
     // load an image
-    SDL_Surface* bmp = SDL_LoadBMP("cb.bmp");
-    if (!bmp)
+    SDL_Surface * bmp;
+    bmp = SDL_LoadBMP("cb.bmp");
+    if (bmp == NULL)
     {
         printf("Unable to load bitmap: %s\n", SDL_GetError());
         return 1;
@@ -35,28 +45,25 @@ int main ( int argc, char** argv )
     dstrect.x = (screen->w - bmp->w) / 2;
     dstrect.y = (screen->h - bmp->h) / 2;
 
-    // program main loop
-    int done = 0;
-    while (!done)
+    while (1)
     {
         // message processing loop
         SDL_Event event;
         while (SDL_PollEvent(&event))
         {
-            // check for messages
             switch (event.type)
             {
-                // exit if the window is closed
+                /* this event indicates the window being closed */
             case SDL_QUIT:
-                done = 1;
+                running = 1;
                 break;
 
-                // check for keypresses
+                /* event for a key being pressed down */
             case SDL_KEYDOWN:
                 {
                     // exit if ESCAPE is pressed
                     if (event.key.keysym.sym == SDLK_ESCAPE)
-                        done = 1;
+                        running = 1;
                     break;
                 }
             }
@@ -66,14 +73,11 @@ int main ( int argc, char** argv )
 
         SDL_BlitSurface(bmp, 0, screen, &dstrect);
 
-        //update the screen
+        /* update the screen */
         SDL_Flip(screen);
-    } // end main loop
+    }
 
-    // free loaded bitmap
     SDL_FreeSurface(bmp);
 
-    // all is well ;)
-    printf("Exited cleanly\n");
     return 0;
 }
